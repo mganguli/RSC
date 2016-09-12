06f80206ca8d09fa37588c6f634325a0c358fe49
Nate Potter <nathaniel.potter@intel.com>
2016-09-12 15:37:46 -0700

    Add API calls to ComposedNodes
    This commit updates the ComposedNodes module to add API calls
    for getting data on composed nodes rather than mocking it.
    
    Signed-off-by: Nate Potter <nathaniel.potter@intel.com>

--- plasma/ui/src/js/components/home/ComposedNodes.js
+++ plasma/ui/src/js/components/home/ComposedNodes.js
@@ -1,20 +1,45 @@
 import React from "react";
-import {mockComposedNodes} from "./MockData";
 import ItemList from "./ItemList";
 
-export default class ComposedNodes extends React.Component {
-  componentWillMount() {
-     var composedNodes = this.getComposedNodes();
-     this.setState({composedNodes: composedNodes});
-   }
+var util = require('./util.js');
 
-   getComposedNodes() {
-     return mockComposedNodes;
-   }
+const ComposedNodes = React.createClass({
+
+  getInitialState() {
+    return {composedNodes: []};
+  },
+
+  componentWillMount() {
+    this.getComposedNodes();
+  },
+
+  getComposedNodes() {
+    var composedNodes;
+    var url = 'http://127.0.0.1:6000/redfish/v1/Nodes';
+    $.ajax({
+      url: url,
+      type: 'GET',
+      dataType: 'json',
+      cache: false,
+      success: function(resp) {
+        composedNodes = util.listMembers(resp);
+        this.setData(composedNodes);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  setData(composedNodes) {
+    this.setState({composedNodes: composedNodes});
+  },
 
   render() {
     return (
       <ItemList items={this.state.composedNodes} header="COMPOSED NODES" />
     );
   }
-}
+});
+
+export default ComposedNodes
