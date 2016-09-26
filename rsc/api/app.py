6d2a70ba854ddd859be5a46705f1d1fc6c3f82a2
Seenivasan Gunabalan <agsnivas@gmail.com>
2016-09-26 11:35:25 +0530

    Added CORS attributes using pecan hooks

--- rsc/api/app.py
+++ rsc/api/app.py
@@ -15,8 +15,8 @@ from oslo_middleware import request_id
 from oslo_service import service
 from pecan import configuration
 from pecan import make_app
+from rsc.api import hooks
 from rsc.common import exceptions as p_excp
-
 
 def setup_app(*args, **kwargs):
     config = {
@@ -35,27 +35,15 @@ def setup_app(*args, **kwargs):
     }
     pecan_config = configuration.conf_from_dict(config)
 
+    app_hooks = [hooks.CORSHook()]
+
     app = make_app(
         pecan_config.app.root,
-        wrap_app=_wrap_app,
+        hooks=app_hooks,
         logging=getattr(config, 'logging', {})
     )
-
     return app
 
-
-def _wrap_app(app):
-    app = request_id.RequestId(app)
-
-    if cfg.CONF.auth_strategy == 'noauth':
-        pass
-    elif cfg.CONF.auth_strategy == 'keystone':
-        p_excp.NotImplemented(func_name='keystone as auth_strategy')
-    else:
-        raise p_excp.InvalidConfigurationOption(
-            opt_name='auth_strategy', opt_value=cfg.CONF.auth_strategy)
-
-    return app
 
 _launcher = None
 
